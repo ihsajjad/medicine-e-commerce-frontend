cd0d8a11ebf1bf40c5950bd98d1057b66e5714fc
Iftekher Hossen Sajjad <iftekherhossensajjad@gmail.com>
2024-08-11 15:29:38 +0600

    Created the navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+
+const NavBar = () => {
+  const isLoggedIn = true;
+
+  const options = (
+    <>
+      <li>
+        <Link href={"/"}>Home</Link>
+      </li>
+      {isLoggedIn ? (
+        <>
+          <li>
+            <button className="border px-2 py-1 rounded-full text-sm border-primary text-primary">
+              Sign Out
+            </button>
+          </li>
+        </>
+      ) : (
+        <>
+          <li>
+            <Link href={"/sign-in"}>Sign In</Link>
+          </li>
+        </>
+      )}
+    </>
+  );
+
+  return (
+    <nav className="h-14 w-full bg-white shadow-xl shadow-[#00000010] sticky top-0 flex-center">
+      <MaxWidthWrapper>
+        <div className="flex items-center justify-between">
+          <Link href={"/"} className="text-2xl font-semibold text-primary">
+            CareCube
+          </Link>
+          <menu className="flex items-center gap-4">{options}</menu>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
+
+export default NavBar;
